04b507aa8051887317de70905db794c44931dea0
lauslim12 <nicholasdwiarto@yahoo.com>
2020-11-22 14:16:06 +0700

    fix: Fixed day count bug

--- utils/parseNotification.js
+++ utils/parseNotification.js
@@ -3,12 +3,20 @@ module.exports = (element, index) => {
     return;
   }
 
+  let differenceInDays;
+
   const currentTime = new Date(Date.now());
   const deadline = new Date(element.deadline);
   const dateDifference = new Date(deadline - currentTime);
 
-  const differenceInDays = dateDifference.getUTCDate();
+  // If the current date is the same as the deadline date, print 'Today'.
+  if (currentTime.toDateString() === deadline.toDateString()) {
+    differenceInDays = 'Today';
+  } else {
+    differenceInDays = `${dateDifference.getUTCDate()} days`;
+  }
+
   const formattedTime = new Date(element.deadline).toISOString().split('T')[0];
 
-  return `${index}. ${element.name} (Deadline: ${formattedTime} — ${differenceInDays} days)`;
+  return `${index}. ${element.name} (Deadline: ${formattedTime} — ${differenceInDays})`;
 };
