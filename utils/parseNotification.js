286c373dca7ea88b71f5bc1c621d80f5b6249da1
lauslim12 <nicholasdwiarto@yahoo.com>
2020-11-23 10:58:05 +0700

    fix: Edited the function to take more parameters for better formatting

--- utils/parseNotification.js
+++ utils/parseNotification.js
@@ -1,4 +1,5 @@
-module.exports = (element, index) => {
+// Types: 'reminder' and 'cleaning up'.
+module.exports = (element, index, type) => {
   if (!element) {
     return;
   }
@@ -9,14 +10,23 @@ module.exports = (element, index) => {
   const deadline = new Date(element.deadline);
   const dateDifference = new Date(deadline - currentTime);
 
-  // If the current date is the same as the deadline date, print 'Today'.
-  if (currentTime.toDateString() === deadline.toDateString()) {
-    differenceInDays = 'Today';
+  // If the current date is the same as the deadline date, AND the type is 'reminder', print 'Today'.
+  // The whitespace in the 'differenceInDays' variable is intentional.
+  if (
+    currentTime.toDateString() === deadline.toDateString() &&
+    type === 'reminder'
+  ) {
+    differenceInDays = ' — Today';
+  } else if (
+    currentTime.toDateString() !== deadline.toDateString() &&
+    type === 'reminder'
+  ) {
+    differenceInDays = ` — ${dateDifference.getUTCDate()} days`;
   } else {
-    differenceInDays = `${dateDifference.getUTCDate()} days`;
+    differenceInDays = '';
   }
 
   const formattedTime = new Date(element.deadline).toISOString().split('T')[0];
 
-  return `${index}. ${element.name} (Deadline: ${formattedTime} — ${differenceInDays})`;
+  return `${index}. ${element.name} (Deadline: ${formattedTime}${differenceInDays})`;
 };
