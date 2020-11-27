55e201f208e688e0ccfa2c34a5285d25192a9d32
lauslim12 <nicholasdwiarto@yahoo.com>
2020-11-27 10:57:20 +0700

    feat: Added error handling for duplicate keys

--- functions/errorFunctions.js
+++ functions/errorFunctions.js
@@ -5,5 +5,11 @@ module.exports = async (err) => {
   const event = err.eventObject;
   const response = createResponse(err.message);
 
+  // Handle duplicate key errors here.
+  // Error code for Mongoose duplicate key is 11000.
+  if (err && err.code === 11000) {
+    return;
+  }
+
   await client.replyMessage(event.replyToken, response);
 };
