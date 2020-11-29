af8025802774c09261903f6f80972dcff155ffe7
lauslim12 <nicholasdwiarto@yahoo.com>
2020-11-29 17:49:11 +0700

    feat: Added error handling for the 'catch' block

--- functions/errorFunctions.js
+++ functions/errorFunctions.js
@@ -2,7 +2,14 @@ const createResponse = require('../utils/createResponse');
 const { client } = require('../utils/credentialHandler');
 
 module.exports = async (err) => {
-  const event = err.eventObject;
+  const event = err.eventObject || {};
+
+  // If the 'event' object is empty, we don't need to reply anything.
+  // Just return silently.
+  if (Object.keys(event).length === 0) {
+    return;
+  }
+
   const response = createResponse(err.message);
 
   await client.replyMessage(event.replyToken, response);
