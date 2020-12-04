33bd75f12f5ae252d3e5c8d2a2327a233e7a1a48
lauslim12 <nicholasdwiarto@yahoo.com>
2020-12-04 10:04:27 +0700

    fix: Changed the words on the error message

--- utils/featureGuard.js
+++ utils/featureGuard.js
@@ -2,10 +2,6 @@ const AppError = require('./appError');
 
 module.exports = (event) => {
   if (event.source.userId !== process.env.ADMIN_USER_ID) {
-    throw new AppError(
-      'Sorry, Anzu does not accept data manipulation orders from anyone other than Nicholas Dwiarto. Please ask him instead.',
-      401,
-      event
-    );
+    throw new AppError('You are unauthorized to use this command!', 401, event);
   }
 };
