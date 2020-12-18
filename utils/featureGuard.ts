e674cb1160fd8156d8c253b089d7cd279fda13d9
lauslim12 <nicholasdwiarto@yahoo.com>
2020-12-18 19:47:09 +0700

    fix: Added guard to the 'featureGuard' function

--- utils/featureGuard.ts
+++ utils/featureGuard.ts
@@ -1,11 +1,16 @@
 import AppError from './appError';
+import { isStringEmpty } from './generalUtilities';
 
 type FeatureGuardType = {
-  userId: string;
+  userId?: string;
   replyToken: string;
 };
 
 const featureGuard = ({ userId, replyToken }: FeatureGuardType): void => {
+  if (isStringEmpty(userId)) {
+    return;
+  }
+
   if (userId !== process.env.ADMIN_USER_ID) {
     throw new AppError(
       'You are unauthorized to use this command!',
