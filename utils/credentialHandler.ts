c6b8f218a2bea04369d71c517c094cf0181f7fec
lauslim12 <nicholasdwiarto@yahoo.com>
2020-12-18 21:24:08 +0700

    fix: Fixed client export error

--- utils/credentialHandler.ts
+++ utils/credentialHandler.ts
@@ -1,13 +1,15 @@
-import line, { ClientConfig, MiddlewareConfig } from '@line/bot-sdk';
+import { ClientConfig, MiddlewareConfig } from '@line/bot-sdk';
 
-export const config: ClientConfig = {
+const line = require('@line/bot-sdk');
+
+const config: ClientConfig = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
 export const client = new line.Client(config);
 
-export const configurations: MiddlewareConfig = {
+const configurations: MiddlewareConfig = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET || '',
 };
