1cd60ec5f6cb2f94e60c42898879af5498d67dd8
lauslim12 <nicholasdwiarto@yahoo.com>
2020-12-21 19:59:53 +0700

    fix: Added more environment variables and its data type

--- environment.d.ts
+++ environment.d.ts
@@ -1,8 +1,12 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      ADMIN_USER_ID: string;
+      CHANNEL_ACCESS_TOKEN: string;
+      CHANNEL_SECRET: string;
       DATABASE: string;
       DATABASE_PASSWORD: string;
+      DATABASE_USERNAME: string;
     }
   }
 }
