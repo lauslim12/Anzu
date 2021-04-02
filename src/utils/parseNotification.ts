2798a0dc67ec1001c51caa4177b3c53583456d2b
lauslim12 <nicholasdwiarto@yahoo.com>
2021-04-02 09:47:13 +0700

    fix: Fixed difference in days calculation mistake

--- src/utils/parseNotification.ts
+++ src/utils/parseNotification.ts
@@ -1,6 +1,15 @@
 import { isObjectEmpty } from './generalUtilities';
 import { ParseNotificationElementType } from '../types';
 
+const millisecondsPerDay = 1000 * 60 * 60 * 24;
+
+const dateDiffInDays = (a: Date, b: Date): number => {
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / millisecondsPerDay);
+};
+
 // Types: 'reminder' and 'cleaning up'.
 const parseNotification = (
   element: ParseNotificationElementType,
@@ -15,7 +24,7 @@ const parseNotification = (
 
   const currentTime = new Date(Date.now());
   const deadline = new Date(element.deadline);
-  const dateDifference = new Date(deadline.getTime() - currentTime.getTime());
+  const dateDifference = dateDiffInDays(currentTime, deadline);
 
   // If the current date is the same as the deadline date, AND the type is 'reminder', print 'Today'.
   // The whitespace in the 'differenceInDays' variable is intentional.
@@ -28,7 +37,7 @@ const parseNotification = (
     currentTime.toDateString() !== deadline.toDateString() &&
     type === 'reminder'
   ) {
-    differenceInDays = ` — ${dateDifference.getUTCDate()} days`;
+    differenceInDays = ` — ${dateDifference} days`;
   } else {
     differenceInDays = '';
   }
