90e30c22bbf14a4ef67d92ce13ad981efc3d16e2
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-25 17:30:45 +0700

    feat: Implement 'startApp' async function

--- src/index.ts
+++ src/index.ts
@@ -4,75 +4,98 @@ import express from 'express';
 
 import environments from './config/environments';
 import loadExpress from './infra/express';
-import { loadLINEMiddleware } from './infra/line';
+import { loadLINEClient, loadLINEMiddleware } from './infra/line';
 import loadMongoDB from './infra/mongodb';
+import TaskModel from './task/schema';
 
 /**
- * Load application.
+ * Starts the whole application.
  */
-const app = loadExpress(express(), loadLINEMiddleware);
+async function startApp() {
+  /**
+   * Load database.
+   */
+  await loadMongoDB();
 
-/**
- * Handles uncaught exceptions to prevent app error before starting.
- */
-process.on('uncaughtException', (err) => {
-  console.log('Unhandled exception 💥! Application shutting down!');
-  console.log(err.name, err.message);
-  process.exit(1);
-});
+  /**
+   * Load application.
+   */
+  const app = loadExpress(
+    express(),
+    loadLINEClient,
+    loadLINEMiddleware,
+    TaskModel
+  );
 
-/**
- * Run application on port that is suited for the environment.
- */
-const server = app.listen(environments.port, () => {
-  console.log(`Express API is now listening on port ${environments.port}!`);
-});
-
-/**
- * Handles unhandled rejections, and shut down gracefully.
- */
-process.on('unhandledRejection', (err) => {
-  console.error('Unhandled rejection 💥! Application shutting down!');
-  console.error(err);
-
-  // Finish all requests that are still pending, the shutdown gracefully.
-  server.close(() => {
+  /**
+   * Handles uncaught exceptions to prevent app error before starting.
+   */
+  process.on('uncaughtException', (err) => {
+    console.log('Unhandled exception 💥! Application shutting down!');
+    console.log(err.name, err.message);
     process.exit(1);
   });
-});
+
+  /**
+   * Run application on port that is suited for the environment.
+   */
+  const server = app.listen(environments.port, () => {
+    console.log(`Express API is now listening on port ${environments.port}!`);
+  });
+
+  /**
+   * Handles unhandled rejections, and shut down gracefully.
+   */
+  process.on('unhandledRejection', (err) => {
+    console.error('Unhandled rejection 💥! Application shutting down!');
+    console.error(err);
+
+    // Finish all requests that are still pending, the shutdown gracefully.
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+
+  /**
+   * Handles 'SIGQUIT' signal.
+   */
+  process.on('SIGQUIT', () => {
+    console.log('Receiving signal to shut down. Shutting down gracefully...');
+
+    server.close(() => {
+      console.log('Server closed successfully!');
+      process.exit(0);
+    });
+  });
+
+  /**
+   * Handles 'SIGTERM' signal.
+   */
+  process.on('SIGTERM', () => {
+    console.log('Receiving signal to terminate. Shutting down gracefully...');
+
+    server.close(() => {
+      console.log('Server closed successfully!');
+      process.exit(0);
+    });
+  });
+
+  /**
+   * Handles 'SIGINT' signal.
+   */
+  process.on('SIGINT', () => {
+    console.log('Receiving signal to interrupt. Shutting down gracefully...');
+
+    server.close(() => {
+      console.log('Server closed successfully!');
+      process.exit(0);
+    });
+  });
+}
 
 /**
- * Handles 'SIGQUIT' signal.
+ * Simulates 'function main' on most programming languages.
  */
-process.on('SIGQUIT', () => {
-  console.log('Receiving signal to shut down. Shutting down gracefully...');
-
-  server.close(() => {
-    console.log('Server closed successfully!');
-    process.exit(0);
-  });
-});
-
-/**
- * Handles 'SIGTERM' signal.
- */
-process.on('SIGTERM', () => {
-  console.log('Receiving signal to terminate. Shutting down gracefully...');
-
-  server.close(() => {
-    console.log('Server closed successfully!');
-    process.exit(0);
-  });
-});
-
-/**
- * Handles 'SIGINT' signal.
- */
-process.on('SIGINT', () => {
-  console.log('Receiving signal to interrupt. Shutting down gracefully...');
-
-  server.close(() => {
-    console.log('Server closed successfully!');
-    process.exit(0);
-  });
-});
+if (require.main === module) {
+  startApp().catch((err) => console.error(err));
+}
