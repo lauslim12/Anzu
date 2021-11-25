9fb9b07b1f15ff5c15c45969df7cf7c1ae604f9a
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-25 23:46:35 +0700

    feat: Implement source creation on listeners

--- src/listener/handler.ts
+++ src/listener/handler.ts
@@ -1,19 +1,41 @@
-import type { WebhookEvent } from '@line/bot-sdk';
+import type { EventSource, WebhookEvent } from '@line/bot-sdk';
 
+import SourceService from '../source/service';
+import getEventMetadata from '../utils/getEventMetadata';
 import responses from './responses';
 
 /**
  * Handles all 'listener' / 'pub/sub' events (joining, following, etc.).
  *
  * @param type - Event type from webhook event
+ * @param source - Event source
+ * @param sourceService - Source service
  * @returns A message as Anzu's response.
  */
-const handleListener = async (type: WebhookEvent['type']) => {
+const handleListener = async (
+  type: WebhookEvent['type'],
+  source: EventSource,
+  sourceService: SourceService
+) => {
+  const { sourceId, sourceType } = getEventMetadata(source);
+
   if (type === 'follow') {
+    await sourceService.createSource({
+      sourceId,
+      sourceType,
+      dateAdded: Date.now(),
+    });
+
     return responses.added();
   }
 
   if (type === 'join') {
+    await sourceService.createSource({
+      sourceId,
+      sourceType,
+      dateAdded: Date.now(),
+    });
+
     return responses.join();
   }
 
